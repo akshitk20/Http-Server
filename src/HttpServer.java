156b8d7a62bbe12c916dcb11d8445de272713fe4
Akshit Khatri <akshitkhatri.20@gmail.com>
2024-06-15 21:01:48 +0530

    added POST handling

--- src/HttpServer.java
+++ src/HttpServer.java
@@ -41,7 +41,7 @@ public class HttpServer {
                     return;
                 }
                 StringTokenizer tokenizer = new StringTokenizer(input);
-                String method = tokenizer.nextToken().toLowerCase();
+                String method = tokenizer.nextToken();
                 String requestedFile = tokenizer.nextToken().toLowerCase();
 
                 // default file to serve
@@ -53,29 +53,65 @@ public class HttpServer {
                 PrintWriter out = new PrintWriter(clientSocket.getOutputStream(),true);
                 BufferedOutputStream outputStream = new BufferedOutputStream(clientSocket.getOutputStream());
                 Path filePath = Paths.get("public", requestedFile);
-                if (Files.exists(filePath)) {
-                    byte[] fileData = Files.readAllBytes(filePath);
-                    String contentType = Files.probeContentType(filePath);
-                    // send HTTP response
-                    out.println("HTTP/1.1 200 OK");
-                    out.println("Content-Type: " + contentType);
-                    out.println("Content-Length: " + fileData.length);
-                    out.println();
-                    out.flush();
-                    outputStream.write(fileData, 0, fileData.length);
-                    outputStream.flush();
+                if (method.equals("GET")) {
+                    serveFile(out, outputStream, filePath);
+                } else if (method.equals("POST") && requestedFile.equals("/submit")) {
+                    handlePostRequest(reader, out);
                 } else {
-
                     // File not found
-                    out.println("HTTP/1.1 404 Not Found");
+                    out.println("HTTP/1.1 405 Method not allowed");
                     out.println("Content-Type: text/html");
                     out.println();
-                    out.println("<h1>404 Not Found</h1>");
+                    out.println("<h1>405 Method not allowed</h1>");
                     out.flush();
                 }
                 System.out.println("Handled request: " + input);
             } catch (IOException e) {
                 throw new RuntimeException(e);
+            }
+        }
+
+        private void handlePostRequest(BufferedReader reader, PrintWriter out) throws IOException {
+            StringBuilder payload = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null && !line.isEmpty()) {
+                payload.append(line).append("\n");
+            }
+            System.out.println("Post payload " + payload);
+            String body = reader.readLine();
+            if (body != null) {
+                payload.append(body);
+            }
+
+            System.out.println("Post payload with body " + payload);
+            String responseMessage = "Received POST data " + payload;
+            out.println("HTTP/1.1 200 OK");
+            out.println("Content-Type: text/html");
+            out.println();
+            out.println("<h1>Form Submission Successful</h1>");
+            out.println("<p>" + responseMessage + "</p>");
+            out.flush();
+        }
+
+        private void serveFile(PrintWriter out, BufferedOutputStream outputStream, Path filePath) throws IOException {
+            if (Files.exists(filePath)) {
+                byte[] fileData = Files.readAllBytes(filePath);
+                String contentType = Files.probeContentType(filePath);
+                // send HTTP response
+                out.println("HTTP/1.1 200 OK");
+                out.println("Content-Type: " + contentType);
+                out.println("Content-Length: " + fileData.length);
+                out.println();
+                out.flush();
+                outputStream.write(fileData, 0, fileData.length);
+                outputStream.flush();
+            } else {
+                // File not found
+                out.println("HTTP/1.1 404 Not Found");
+                out.println("Content-Type: text/html");
+                out.println();
+                out.println("<h1>404 Not Found</h1>");
+                out.flush();
             }
         }
     }
