478f4a1e360ff051011b2dfaa5cdf8366dbe9d15
Akshit Khatri <akshitkhatri.20@gmail.com>
2024-06-16 18:28:59 +0530

    fix post form

--- src/HttpServer.java
+++ src/HttpServer.java
@@ -1,9 +1,13 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.StringTokenizer;
 
 public class HttpServer {
@@ -74,14 +78,28 @@ public class HttpServer {
         private void handlePostRequest(BufferedReader reader, PrintWriter out) throws IOException {
             StringBuilder payload = new StringBuilder();
             String line;
-            while ((line = reader.readLine()) != null && !line.isEmpty()) {
-                payload.append(line).append("\n");
+//            while ((line = reader.readLine()) != null && !line.isEmpty()) {
+//                payload.append(line).append("\n");
+//            }
+            int contentLength = 0;
+            while (!(line = reader.readLine()).isEmpty()) {
+                if (line.startsWith("Content-Length:")) {
+                    contentLength = Integer.parseInt(line.substring("Content-Length:".length()).trim());
+                }
             }
-            System.out.println("Post payload " + payload);
+
+            // Read the body
+            char[] charArray = new char[contentLength];
+            reader.read(charArray);
+            String body = new String(charArray);
+            System.out.println("Post payload " + body);
+
+            // Parse form data
+            Map<String, String> formData = parseFormData(body);
 
 
-            System.out.println("Post payload with body " + payload);
-            String responseMessage = "Received POST data " + payload;
+            System.out.println("Post payload with body " + formData);
+            String responseMessage = "Received POST data " + formData;
             out.println("HTTP/1.1 200 OK");
             out.println("Content-Type: text/html");
             out.println();
@@ -111,5 +129,19 @@ public class HttpServer {
                 out.flush();
             }
         }
+
+        private Map<String, String> parseFormData(String body) {
+            Map<String, String> formData = new HashMap<>();
+            String[] pairs = body.split("&");
+            for (String pair : pairs) {
+                String[] keyValue = pair.split("=");
+                if (keyValue.length == 2) {
+                    formData.put(URLDecoder.decode(keyValue[0], StandardCharsets.UTF_8), URLDecoder.decode(keyValue[1],
+                            StandardCharsets.UTF_8));
+                }
+            }
+            return formData;
+        }
+
     }
 }
