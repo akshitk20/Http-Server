74246914655544a4eefb23f077171f5d95418d54
Akshit Khatri <akshitkhatri.20@gmail.com>
2024-07-07 20:27:30 +0530

    add connection code for db

--- src/config/DatabaseConnection.java
+++ src/config/DatabaseConnection.java
@@ -1,6 +1,8 @@
 package config;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DatabaseConnection {
 
@@ -8,7 +10,27 @@ public class DatabaseConnection {
     private static final String USERNAME = "admin";
     private static final String PASSWORD = "password";
     private static Connection connection;
+    public DatabaseConnection() {}
 
-    
+    public Connection getConnection() {
+        if (connection == null) {
+            try {
+                connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return connection;
+    }
+
+    public void closeConnection() {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
 }
