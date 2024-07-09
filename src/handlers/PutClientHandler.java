d0184c272ef85464d100efe5cebfed655f04e5f7
Akshit Khatri <akshitkhatri.20@gmail.com>
2024-07-09 20:24:14 +0530

    added logging for PUT method

--- src/handlers/PutClientHandler.java
+++ src/handlers/PutClientHandler.java
@@ -5,16 +5,21 @@ import org.json.JSONObject;
 import java.io.*;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class PutClientHandler implements RouteHandler {
+    private static final Logger logger = Logger.getLogger(PutClientHandler.class.getName());
+
     @Override
     public void handle(String path, String method,
                        BufferedReader reader, PrintWriter out,
                        Connection connection) throws IOException {
         // handle put handling
-        System.out.println("Starting PUT method");
+        logger.info("Starting PUT method");
         File file = new File("public/" + path);
         if (path.contains("/items")) {
+            logger.info("starting PUT /items endpoint");
             String[] parts = path.split("/");
             if (parts.length == 3 && "items".equals(parts[1])) {
                 try {
@@ -44,12 +49,14 @@ public class PutClientHandler implements RouteHandler {
                         out.println("Content-Type: application/json");
                         out.println();
                         out.println(jsonObject);
+                        logger.info("PUT completed for id " + id);
                     } else {
+                        logger.info("PUT not completed due to invalid id");
                         out.println("HTTP/1.1 404 NOT FOUND");
                         out.println();
                     }
                 } catch (Exception e) {
-                    e.printStackTrace();
+                    logger.log(Level.SEVERE, e.getMessage(), e);
                     // send response to client
                     out.println("HTTP/1.1 500 Internal Server Error");
                     out.println("Content-Type: application/json");
@@ -59,6 +66,7 @@ public class PutClientHandler implements RouteHandler {
 
             }
         } else {
+            logger.info("starting file content update endpoint");
             try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
                 // Read and discard the request headers
                 String line;
@@ -81,8 +89,9 @@ public class PutClientHandler implements RouteHandler {
                 out.println();
                 out.println("Resource updated successfully.");
                 out.flush();
+                logger.info("completed file updation");
             } catch (IOException e) {
-                e.printStackTrace();
+                logger.log(Level.SEVERE, e.getMessage(), e);
                 // send response to client
                 out.println("HTTP/1.1 500 Internal Server Error");
                 out.println("Content-Type: text/plain");
